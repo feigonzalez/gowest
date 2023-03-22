e76d8c2213e93c853e0a4be36b7323c3a75bc594
ivanjhm <101402648+ivanjhm@users.noreply.github.com>
2023-03-22 16:47:07 -0300

    Upgraded ihtml.js
    ihtml.js can now execute code included in script tags from the imported html files

--- ihtml.js
+++ ihtml.js
@@ -1,12 +1,22 @@
-//Finds all <iHTML> elements in the main HTML document. For reach, fetches an HTML file,
-//as specified by its src attribute, and loads its content into that iHTML element.
+/*
+    Finds all <iHTML> elements in the main HTML document. For reach, fetches an HTML file,
+    as specified by its src attribute, and loads its content into that iHTML element.
+    After all IHTML contents have been loaded, it executes the contents of all imported
+    <script> tags, using eval(). It does not properly import external scripts declared with
+    <script> tags
+*/
 async function loadAll(){
     for(var toI of document.getElementsByTagName("IHTML")){
         await fetch(toI.getAttribute("src"))
             .then(response => response.text())
             .then(data => toI.innerHTML=data);
     }
+    return;
 }
 window.addEventListener("load",()=>{
-    loadAll()
+    loadAll().then(done =>{
+        for(s of document.querySelectorAll("ihtml script")){
+            eval(s.innerHTML)
+        }
+    })
 })
