4eb3b2d3859f86b32cdac5f9ded37497bdaf4e11
feigonzalez <fei.gonzalezg@duocuc.cl>
2023-03-31 23:36:18 -0300

    Added simple test database. Added db.json to be used for testing. Renamed product images. Index gallery, category, and product pages now retrieve data from the database.

--- gowest.js
+++ gowest.js
@@ -1,26 +1,61 @@
 /*
     Código para ser usado en el proyecto GOWEST, del curso de Programación Web 2023-1
 */
+var debugging=true;
+function debugLog(s){if(debugging)console.log(s)}
 function get(id){return document.getElementById(id)}
 
-//categoryContents={}
-//var g=get("gallery")
-function loadCategoryGalleryInto(cat,e,link){
-    fetch("categoryContents.json").
-        then(response=>response.json()).
-        then(text=>e.innerHTML=parseCategoryContentsJson(text,cat,link))
+async function selectAll(){
+	debugLog("Retrieving DB data...");
+	var res;
+	await fetch("db.json").then(r=>r.json()).then(j=>res=j);
+	debugLog("DB data retrieved.")
+	return res;
 }
 
-function parseCategoryContentsJson(json,cat="",link=false){
-    var c=json["categories"][cat];
+async function selectAllFrom(table){
+	debugLog(`Retrieving data from DB at table [${table}]`);
+	var db = await selectAll();
+	if(table in db){
+		debugLog(`Data from table [${table}] retrieved.`);
+		return db[table];
+	} else {
+		debugLog(`Table [${table}] not found`);
+		return null;
+	}
+}
+
+//compFunc should be given as (i)=>{return X},
+//where i a row of the selected table and
+//X is the comparison function. It should use fields of i
+async function selectAllWhere(table,compFunc){
+	debugLog(`Retrieving data from DB at table [${table}] where [${compFunc}]`)
+	var res=[];
+	var db = await selectAll();
+	if(!(table in db)){
+		debugLog(`Table [${table}] not found.`)
+		return [];
+	} else {
+		for(var i of db[table]){
+			if(compFunc(i))res.push(i)
+		}
+		debugLog(`Retrieved ${res.length} rows from table [${table}]`);
+	}
+	return res;
+}
+
+async function loadCategoryGalleryInto(cat="",e,link=false){
+	var cn = (await selectAllWhere("categories",(i)=>{return i["code"]==cat}))[0]["name"]
+	var c = await selectAllWhere("products",(i)=>{return i["category"]==cat});
+    //var c=json["categories"][cat];
     var cl=c.length //category length
     r="<div class='row bg-gowest categoryTitle'>"+
         (link?"<a href='category.html?c="+cat+"'>":"")+
-        cat+
+        cn+
         (link?" <span class='productCount'>Ver "+cl+" productos</span></a>":"")+"</div>";
     var template=
         "<div class='galleryItem'>"+
-		"<a class='galleryItemLink' href='product.html?id={ID}'>"+
+		"<a class='galleryItemLink' href='product.html?p={ID}'>"+
         "<img class='galleryItemImage mx-auto d-block' src='img/products/{ID}.png'>"+
         "<div class='galleryItemLabel'>{CONTENT}</div>"+
         "<div class='galleryItemPrice'>{PRICE}</div></a>"+
@@ -33,7 +68,7 @@ function parseCategoryContentsJson(json,cat="",link=false){
             if(index<cl){
                 item=c[index]
                 content=template;
-                content=content.replaceAll("{ID}",item["name"])
+                content=content.replaceAll("{ID}",item["code"])
                 content=content.replaceAll("{CONTENT}",item["name"])
                 content=content.replaceAll("{PRICE}","$"+item["price"])
             }
@@ -41,15 +76,7 @@ function parseCategoryContentsJson(json,cat="",link=false){
         }
         r+="</div>";
     }
-    return r;
-}
-
-async function getCategories(){
-    var c;
-    await fetch("categoryContents.json").
-        then(response=>response.json()).
-        then(json=>c=Object.keys(json["categories"]))
-    return c;
+    e.innerHTML=r;
 }
 
 window.addEventListener("load",()=>{
