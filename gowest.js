e8814af0ecc21de1ba07e9f79ee242583abbe9b5
feigonzalez <fei.gonzalezg@duocuc.cl>
2023-04-18 15:11:35 -0400

    Started validation code
    Started validation code on signup
    Started form validation plan documentation

--- gowest.js
+++ gowest.js
@@ -398,5 +398,24 @@ function addToCart(units){
 	params["cart"]=newUnits;
 }
 
+/*
+	Form validation
+*/
+function validateForm(e,ev){
+	ev.preventDefault();
+	switch (e.id){
+		case "signupForm":
+			var pass=get("f_pass").value;
+			var passConfirm=get("f_passConfirm").value;
+			if(pass!=passConfirm){
+				console.log("pass bad")
+				return false;
+			}
+			break;
+		default:break;
+	}
+	e.submit();
+}
+
 window.addEventListener("load",()=>{
 })
